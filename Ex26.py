3f2c5aa6273b33b8f45f1996b7bc54a6b15bee1d
Nattick <natalia.a.konovalova@gmail.com>
2023-04-22 15:01:16 +0300

    Add Ex26

---
+++ Ex26.py
@@ -0,0 +1,11 @@
+# Напишите программу, которая на вход принимает два числа A и B, 
+# и возводит число А в целую степень B с помощью рекурсии.
+
+def power(base, exp):
+    if (exp == 1):
+        return (base)
+    if (exp != 1):
+        return (base * power(base, exp - 1))
+base = int(input("Enter any number: "))
+exp = int(input("Now enter its power: "))
+print("And here is your result:", power(base, exp))
